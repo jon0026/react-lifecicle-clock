b25a2619f6fc61cd6d675f14de64d76d91743683
Juma81 <jonathanumana10@gmail.com>
2022-03-14 21:36:22 -0600

    taller 9 clock

---
+++ src/components/Clock.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import './clock.css';
+
+class Clock extends React.Component {
+
+    timerId = 0;
+
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date(), day: true};
+        console.log('constructor called!');
+    }
+
+    render() {
+        console.log('render called!');
+        return (
+            <div className={`clock ${ !this.state.day? 'night': '' }`}>
+                <h1>Hello, World!</h1>
+                <h2>It's {this.state.date.toLocaleTimeString() } ⏰</h2>
+            </div>
+        );
+    }
+                
+    componentDidMount() {
+        console.log('componentDidMount called!');
+        this.timerId = setInterval(() => {
+            this.tick()
+        }, 1000);
+    }
+
+    tick() {
+        console.log('tick called!');
+        this.setState({
+            date: new Date(),
+            day: this.state.date <= 19
+        });
+    }
+
+    componentDidUpdate() {
+        // console.log('componentDidUpdate called!');
+        if (this.state.day) {
+            console.log('good morning!  🌄');
+        } else {
+            console.log('good night!  🌛');
+        }
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount called!');
+        clearInterval(this.timerId);
+    }
+
+}
+
+
+
+// function Clock(props) {
+//     return (
+//         <div>
+//       <h1>Hello, World!</h1>
+//       <h2>It's {props.date.toLocaleTimeString() } ⏰</h2>
+//     </div>
+//     );
+// }
+
+export default Clock;
+
+    
